a789239a995611102028faa04d0556e809aed3fe
zhangdi <zhangdi@ngarihealth.com>
2022-01-19 14:00:06 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Z_di',
+  description: '描述描述',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '笔记', link: '/note/' },
+      // { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
