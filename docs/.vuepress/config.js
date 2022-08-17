4ae6a88464ccc067ffb548a7397f7f256329e0fc
zhangdi <zhangdi@ngarihealth.com>
2022-08-17 11:18:40 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,28 @@
 module.exports = {
-  base:'/docs/',
+  base: '/docs/',
   title: 'Scorp',
   description: '个人文档、笔记归纳记录',
   themeConfig: {
     sidebar: 'auto',
     nav: [
       { text: '笔记', link: '/note/' },
+      { text: 'API', link: '/api/' },
       { text: 'Github', link: 'https://github.com/scorpdi/judi' },
     ],
-    sidebar:{
-      '/guide/':[
+    sidebar: {
+      '/guide/': [
         '/guide/',
         '/guide/one'
       ],
-      '/note/':[
+      '/api/': [
+        '/api/',
+        '/api/scorp'
+      ],
+      '/note/': [
         {
           title: "计算机网络相关",
           collapsable: false,
-          path:'/note/network',
+          path: '/note/network',
           children: [
             '/note/network/UDP&TCP',
             '/note/network/http&https'
@@ -26,7 +31,7 @@ module.exports = {
         {
           title: "部署",
           collapsable: false,
-          path:'/note/deploy',
+          path: '/note/deploy',
           children: [
             '/note/deploy/gitlab',
             '/note/deploy/docker'
@@ -35,16 +40,16 @@ module.exports = {
         {
           title: "JavaScript",
           collapsable: false,
-          path:'/note/javascript',
+          path: '/note/javascript',
           children: [
-            '/note/javascript/test',
+            '/note/javascript/设计模式',
             // '/note/deploy/docker'
           ],
         },
         {
           title: "Node.js",
           collapsable: false,
-          path:'/note/nodejs',
+          path: '/note/nodejs',
           children: [
             '/note/nodejs/sequelize',
             // '/note/deploy/docker'
---
+++ docs/api/scorp.md
@@ -0,0 +1,20 @@
+# node-service
+
+> Scorp小程序相关接口记录
+> profix - api 
+
+## 登录 **`/auth/signin`**
+- 请求方式： GET
+- 请求参数：
+  - **userName** - string
+  - **passWord** - string
+  - **openid** - string
+  - **nikeName** - string
+- 返回参数：
+  - **userName** - string
+  - **passWord** - string
+  - **openid** - string
+  - **nikeName** - string
+## 注册
+
+## 查询
